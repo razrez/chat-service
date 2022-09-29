944d893b9c085c718dc0d349b8347e17dc23ecae
Руслан Русланов <sharp_blade666@mail.ru>
2022-09-29 17:15:43 +0300

    c sharp Hub

---
+++ src/ChatService/ChatService/Hubs/ChatHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace ChatService.Hubs;
+
+public class ChatHub : Hub
+{
+    public async Task JoinRoom(UserConnection userConnection)
+    {
+        await Groups.AddToGroupAsync(Context.ConnectionId, userConnection.Room);
+        await Clients.Group(userConnection.Room).SendAsync("ReceiveMessage", $"{userConnection.User} has joined {userConnection.Room}");
+    }
+
+}
---
+++ src/ChatService/ChatService/Program.cs
@@ -0,0 +1,37 @@
+using ChatService;
+using ChatService.Hubs;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSignalR();
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(corsPolicyBuilder =>
+    {
+        corsPolicyBuilder.WithOrigins("http://localhost:3000")
+            .AllowAnyHeader()
+            .AllowAnyMethod()
+            .AllowCredentials();
+    });
+});
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseHsts();
+}
+
+/*app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();*/
+
+app.UseRouting();
+
+app.UseCors();
+
+
+app.MapGet("/", () => "Hello World!");
+app.MapHub<ChatHub>("/chat");
+
+app.Run();
