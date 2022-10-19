c0f4c3b0cd79cafd9e8a235254f6f6c15aa0dba0
Руслан Русланов <sharp_blade666@mail.ru>
2022-10-19 20:48:37 +0300

    Consumer and Publisher host is rabbitmq

--- src/chat.back/Chat/Chat.BackgroundService/Consumer.cs
+++ src/chat.back/Chat/Chat.BackgroundService/Consumer.cs
@@ -26,7 +26,7 @@ public class Consumer : Microsoft.Extensions.Hosting.BackgroundService
     {
         _connectionFactory = new ConnectionFactory
         {
-            HostName = "localhost",
+            HostName = "rabbitmq",
         };
         
         _connection = _connectionFactory.CreateConnection();
