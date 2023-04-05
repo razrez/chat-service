b663585ddc61e3dd825d3a6bae8fc22dd11a68d0
Daniil <dddedashev@mail.ru>
2023-04-06 01:05:47 +0300

    типо суета

--- src/spotify.android/SpotifyChat/app/src/main/java/com/example/spotifychat/startactivity/SecondFragmentAuthorization.kt
+++ src/spotify.android/SpotifyChat/app/src/main/java/com/example/spotifychat/startactivity/SecondFragmentAuthorization.kt
@@ -5,13 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
 import com.example.spotifychat.R
-import com.example.spotifychat.databinding.FragmentSecondBinding
+import com.example.spotifychat.databinding.FragmentAuthorizationBinding
 
 class SecondFragmentAuthorization : Fragment() {
 
-    private var _binding: FragmentSecondBinding? = null
+    private var _binding: FragmentAuthorizationBinding? = null
 
 
     private val binding get() = _binding!!
@@ -21,19 +20,10 @@ class SecondFragmentAuthorization : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
-        _binding = FragmentSecondBinding.inflate(inflater, container, false)
+        _binding = FragmentAuthorizationBinding.inflate(inflater, container, false)
         return binding.root
 
     }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        binding.buttonSecond.setOnClickListener {
-            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
-        }
-    }
-
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
--- src/spotify.android/SpotifyChat/app/src/main/java/com/example/spotifychat/startactivity/StartFragmentRegistration.kt
+++ src/spotify.android/SpotifyChat/app/src/main/java/com/example/spotifychat/startactivity/StartFragmentRegistration.kt
@@ -5,13 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
-import com.example.spotifychat.R
-import com.example.spotifychat.databinding.FragmentFirstBinding
+import com.example.spotifychat.databinding.FragmentRegistrationEmailBinding
 
 class StartFragmentRegistration : Fragment() {
 
-    private var _binding: FragmentFirstBinding? = null
+    private var _binding: FragmentRegistrationEmailBinding? = null
 
     private val binding get() = _binding!!
 
@@ -20,17 +18,9 @@ class StartFragmentRegistration : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
-        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+        _binding = FragmentRegistrationEmailBinding.inflate(inflater, container, false)
         return binding.root
 
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        binding.buttonFirst.setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
-        }
     }
 
     override fun onDestroyView() {
