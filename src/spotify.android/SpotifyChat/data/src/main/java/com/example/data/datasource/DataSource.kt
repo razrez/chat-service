6046165c5bab44663efb8c0b3c367bf102f2c61d
Руслан Русланов <sharp_blade666@mail.ru>
2023-05-03 22:10:48 +0300

    apolloClient

--- src/spotify.android/SpotifyChat/data/src/main/java/com/example/data/datasource/DataSource.kt
+++ src/spotify.android/SpotifyChat/data/src/main/java/com/example/data/datasource/DataSource.kt
@@ -1,10 +1,14 @@
 package com.example.data.datasource
 
+import com.apollographql.apollo3.ApolloClient
 import com.example.core.http_clients.RetrofitClient.Companion.retrofitClient
 import com.example.domain.datasource.IDataSourceRetrofit
 
 class DataSource {
     companion object{
         val tokenService: IDataSourceRetrofit = retrofitClient.create(IDataSourceRetrofit::class.java)
+        val apolloClient: ApolloClient = ApolloClient.Builder()
+            .serverUrl("https://localhost:7030/graphq")
+            .build()
     }
 }
