807184d9a945fc464699b43988369f7601413019
Руслан Русланов <sharp_blade666@mail.ru>
2023-05-09 21:34:32 +0300

    simpe client

---
+++ src/chat.back/Chat/GrpcClientTester/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Chat.AppCore;
+using Grpc.Net.Client;
+
+using var channel = GrpcChannel.ForAddress("http://localhost:5167");
+var client = new GrpcClientTester.Chat.ChatClient(channel);
+// get own message
+// var reply = await client.ChatMessaging();
+Console.WriteLine("Press any key to exit...");
+Console.ReadKey();
+Console.WriteLine("Hello, World!");
