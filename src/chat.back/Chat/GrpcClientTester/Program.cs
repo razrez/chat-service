8708c02cb2a4e7841fd7f13762b23355222cbaaa
Руслан Русланов <sharp_blade666@mail.ru>
2023-05-10 20:56:07 +0300

    c sharp GRPC clients work good

--- src/chat.back/Chat/GrpcClientTester/Program.cs
+++ src/chat.back/Chat/GrpcClientTester/Program.cs
@@ -6,11 +6,13 @@ using Grpc.Net.Client;
 Console.WriteLine("Enter your name: ");
 var userName = Console.ReadLine();
 
+Console.WriteLine("name accepted");
 using var channel = GrpcChannel.ForAddress("http://localhost:5038");
 var client = new ChatRoom.ChatRoomClient(channel);
 
 using (var chat = client.join())
 {
+    // вывод получаемых сообщений
     _ = Task.Run(async () =>
     {
         while (await chat.ResponseStream.MoveNext(cancellationToken: CancellationToken.None))
@@ -19,9 +21,11 @@ using (var chat = client.join())
             Console.WriteLine($"{response.User}: {response.Text}");
         }
     });
-
+    
+    // отправка остальным уведомления о том, что ты присоединился 
     await chat.RequestStream.WriteAsync(new Message { User = userName, Text = $"{userName} has joined the room" });
-
+    
+    // выход из чата при вводе "bye"
     string? line;
     while ((line = Console.ReadLine()) != null)
     {
