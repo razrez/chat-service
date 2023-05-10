7592caa90f4ea9309d58d2390b4e6b24cb829e78
Руслан Русланов <sharp_blade666@mail.ru>
2023-05-10 20:23:40 +0300

    я не шарю за асинхронщину...

--- src/chat.back/Chat/GrpcClientTester/Program.cs
+++ src/chat.back/Chat/GrpcClientTester/Program.cs
@@ -3,40 +3,38 @@
 using Chat;
 using Grpc.Net.Client;
 
-Task.Run(async () =>
+Console.WriteLine("Enter your name: ");
+var userName = Console.ReadLine();
+
+using var channel = GrpcChannel.ForAddress("http://localhost:5038");
+var client = new ChatRoom.ChatRoomClient(channel);
+
+using (var chat = client.join())
 {
-    Console.WriteLine("Enter your name: ");
-    var userName = Console.ReadLine();
-
-    using var channel = GrpcChannel.ForAddress("http://localhost:5038");
-    var client = new ChatRoom.ChatRoomClient(channel);
-
-    using (var chat = client.join())
+    _ = Task.Run(async () =>
     {
-        _ = Task.Run(async () =>
+        while (await chat.ResponseStream.MoveNext(cancellationToken: CancellationToken.None))
         {
-            while (await chat.ResponseStream.MoveNext(cancellationToken: CancellationToken.None))
-            {
-                var response = chat.ResponseStream.Current;
-                Console.WriteLine($"{response.User}: {response.Text}");
-            }
-        });
-
-        await chat.RequestStream.WriteAsync(new Message { User = userName, Text = $"{userName} has joined the room" });
-
-        string? line;
-        while ((line = Console.ReadLine()) != null)
-        {
-            if (line.ToLower() == "bye")
-            {
-                break;
-            }
-            await chat.RequestStream.WriteAsync(new Message { User = userName, Text = line });
+            var response = chat.ResponseStream.Current;
+            Console.WriteLine($"{response.User}: {response.Text}");
         }
-        await chat.RequestStream.CompleteAsync();
-    }
+    });
 
-    Console.WriteLine("Disconnecting");
-    await channel.ShutdownAsync();
-});
+    await chat.RequestStream.WriteAsync(new Message { User = userName, Text = $"{userName} has joined the room" });
+
+    string? line;
+    while ((line = Console.ReadLine()) != null)
+    {
+        if (line.ToLower() == "bye")
+        {
+            break;
+        }
+        await chat.RequestStream.WriteAsync(new Message { User = userName, Text = line });
+    }
+    await chat.RequestStream.CompleteAsync();
+}
+
+Console.WriteLine("Disconnecting");
+await channel.ShutdownAsync();
+
 
