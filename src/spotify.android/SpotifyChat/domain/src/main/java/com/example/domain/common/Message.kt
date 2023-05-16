32aaa064d62038c4bce79aeff537bd543c3e7936
Руслан Русланов <sharp_blade666@mail.ru>
2023-05-17 01:34:35 +0300

    нужно маппить сообщения с бека, тк как там другие поля

--- src/spotify.android/SpotifyChat/domain/src/main/java/com/example/domain/common/Message.kt
+++ src/spotify.android/SpotifyChat/domain/src/main/java/com/example/domain/common/Message.kt
@@ -2,7 +2,7 @@ package com.example.domain.common
 
 import android.graphics.Bitmap
 import kotlinx.serialization.Serializable
-
+// TODO("нужен маппер, потому что у сообщений с бека немного другие поля")
 data class Message(
     val message: String,
     val sender: User?,
