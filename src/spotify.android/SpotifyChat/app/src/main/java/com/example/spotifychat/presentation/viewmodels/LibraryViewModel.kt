ed9c46b12f456f7c04b92837b27f1da5e80425cd
Daniil <dddedashev@mail.ru>
2023-06-06 15:23:04 +0300

    че то пытался rabbit но не все (крашит страницу library)

--- src/spotify.android/SpotifyChat/app/src/main/java/com/example/spotifychat/presentation/viewmodels/LibraryViewModel.kt
+++ src/spotify.android/SpotifyChat/app/src/main/java/com/example/spotifychat/presentation/viewmodels/LibraryViewModel.kt
@@ -3,9 +3,12 @@ package com.example.spotifychat.presentation.viewmodels
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.core.http_clients.RabbitMqClient
 import com.example.data.SongsQuery
 import com.example.data.usecases.SongsUseCase
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class LibraryViewModel : ViewModel() {
     private val songsUseCase = SongsUseCase()
@@ -15,6 +18,15 @@ class LibraryViewModel : ViewModel() {
         viewModelScope.launch {
             val songsData = songsUseCase.getSongs()
             songsMutableList.postValue(songsData as List<SongsQuery.Node>?)
+        }
+    }
+
+    fun getStats() {
+        viewModelScope.launch {
+
+            val rabbit = RabbitMqClient()
+
+            rabbit.defaultExchangeAndQueue().startListening()
         }
     }
 }
---
+++ src/spotify.android/SpotifyChat/core/src/main/java/com/example/core/http_clients/RabbitMqClient.kt
@@ -0,0 +1,93 @@
+package com.example.core.http_clients
+
+import android.util.Log
+import com.rabbitmq.client.*
+import java.nio.charset.StandardCharsets
+
+class RabbitMqClient {
+    private var rabbitmq: Connection? = null;
+
+    init {
+        val factory = ConnectionFactory()
+        factory.username = "guest"
+        factory.password = "guest"
+        factory.virtualHost = "/"
+        factory.host = "10.0.2.2"
+        factory.port = 15672
+        rabbitmq = factory.newConnection()
+    }
+
+    fun defaultExchangeAndQueue(): RabbitMqClient {
+        val channel = rabbitmq?.createChannel()
+
+        channel?.exchangeDeclare("exchange", "fanout", true)
+        channel?.queueDeclare("queue", true, false, true, emptyMap())
+        channel?.queueBind("queue", "exchange", "mykey")
+
+        channel?.close()
+        rabbitmq?.close()
+
+        return this
+    }
+
+    fun startListening() {
+        val channel = rabbitmq?.createChannel()
+        val deliverCallback = DeliverCallback{ consumerTag: String?, message: Delivery? ->
+            println("Consume message: ${String(message!!.body)}")
+        }
+        val cancelCallback = CancelCallback { consumerTag: String? -> println("Cancelled... $consumerTag") }
+
+        channel?.basicConsume("queue", true, deliverCallback, cancelCallback)
+
+    }
+
+    fun consumeLogout(queueName: String, session: String, callback: (msg: String) -> Unit) {
+        println("I'm ok")
+        val channel = rabbitmq!!.createChannel()
+
+        val deliverCallback = DeliverCallback { consumerTag: String?, delivery: Delivery ->
+            val message = String(delivery.body, StandardCharsets.UTF_8)
+            println("[$consumerTag] Received message: '$message'")
+            if ( message == session) {
+                callback(message)
+                channel.basicAck(delivery.envelope.deliveryTag, false)
+            }
+
+        }
+        val cancelCallback = CancelCallback { consumerTag: String? ->
+            println("[$consumerTag] was canceled")
+        }
+
+        try {
+            channel.basicConsume(queueName, true, "", deliverCallback, cancelCallback)
+        }
+        catch(e: Exception) {
+            Log.e("errrrrrr", e.message.toString())
+        }
+    }
+
+    fun consumeSome(callback: (msg: String) -> Unit)  {
+        val channel = rabbitmq!!.createChannel()
+
+        val deliverCallback = DeliverCallback { consumerTag: String?, delivery: Delivery ->
+            val message = String(delivery.body, StandardCharsets.UTF_8)
+            println("[$consumerTag] Received message: '$message'")
+            callback(message)
+
+        }
+        val cancelCallback = CancelCallback { consumerTag: String? ->
+            println("[$consumerTag] was canceled")
+        }
+
+        try {
+            channel.basicConsume("some", true, "", deliverCallback, cancelCallback)
+        }
+        catch(e: Exception) {
+            Log.e("errrrrrr", e.message.toString())
+        }
+    }
+
+    fun close() {
+        rabbitmq!!.abort()
+    }
+}
